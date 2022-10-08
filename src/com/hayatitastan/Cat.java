f40e2659c6d3ce20fcbb685462ba0527a07956d9
hayyam <haytastan@gmail.com>
2022-10-08 11:35:32 +0300

    java_oo_abtraction1_extends_abstractclass_ht

---
+++ src/com/hayatitastan/Cat.java
@@ -0,0 +1,16 @@
+package com.hayatitastan;
+
+/**
+ * Created by Dr. Hayati TASTAN on 6.10.2022
+ **/
+public class Cat extends Animal {
+
+  public void animalSound() {
+    // The body of animalSound() is provided here
+    System.out.println("The cat sounds: miyav");
+  }
+  public void sleep() {
+    // The body of sleep() is provided here
+    System.out.println("The cat sleeps: zzzzzz");
+  }
+}
